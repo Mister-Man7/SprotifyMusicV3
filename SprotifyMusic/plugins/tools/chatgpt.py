85c223000d015096c2613742beec10ea504347e8
Mister-Man7 <muhamadamanudin6@gmail.com>
2024-08-19 19:05:38 +0700

    Add ChatGPT

--- SprotifyMusic/plugins/tools/chatgpt.py
+++ SprotifyMusic/plugins/tools/chatgpt.py
@@ -1,16 +1,15 @@
 from pyrogram import filters
-from pyrogram.types import InlineKeyboardMarkup
 import requests
 
 from SprotifyMusic import app
-from pyrogram.enums import ChatAction
+from pyrogram.enums import ChatAction, ParseMode
 from MukeshAPI import api
 
 @app.on_message(filters.command(["chatgpt", "ask", "gpt", "ai"]))
 async def gpt_bot(m, message):
     if len(message.command) < 2:
             await message.reply_text(
-            "Example:**\n\n`/chatgpt write simple website code using html css ,js?`")
+            "**Example:**\n\n`/chatgpt write simple website code using html css ,js?`")
     else:
         a = message.text.split(' ', 1)[1]
 
@@ -19,7 +18,7 @@ async def gpt_bot(m, message):
         await m.send_chat_action(message.chat.id, ChatAction.TYPING)
         x=response["results"]
         
-        await message.reply_text(f"{x}\n\n**😎Powered by @SprotifyMusicBot **", quote=True)  
+        await message.reply_text(f"{x}\n\n**😎Powered by @SprotifyMusicBot**", ParseMode.MARKDOWN)
         
             
     except requests.exceptions.RequestException as e:
